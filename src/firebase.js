4a9e1f693019036a003550128eda1dc8043e3008
jacekgawlyta <jac.gawlyta@gmail.com>
2024-02-17 00:15:35 +0100

    Fix bug in firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
 // Import the functions you need from the SDKs you need
 // const dotenv = require('dotenv');
-
-import React from "react";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
@@ -15,23 +12,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  // apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  // authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-  // projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-  // storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-  // messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-  // appId: import.meta.env.VITE_FIREBASE_APPID,
-  // measurementId: import.meta.env.VITE_FIREBASE_MEASUREMEN
-  apiKey: "AIzaSyDsuwZp0PW8Y-Shhlo7xOCdQ1w2XCtxqkE",
-  authDomain: "project-manager-10c79.firebaseapp.com",
-  projectId: "project-manager-10c79",
-  storageBucket: "project-manager-10c79.appspot.com",
-  messagingSenderId: "438765228634",
-  appId: "1:438765228634:web:5424ccb5230a7d48fbfe63",
-  measurementId: "G-LNC1FS82W7"
-
-  
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMEN
 };
 
 // Initialize Firebase
